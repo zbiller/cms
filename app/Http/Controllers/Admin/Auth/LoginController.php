b3a4a7201e202dcf9a4fe2c2ff627863060959e3
Andrei Badea <zbiller@gmail.com>
2017-02-22 19:59:33 +0200

    override login throttling settings: max attempts & lockout time

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -15,6 +16,20 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = '/admin';
+
+    /**
+     * The number of tries a user is allowed to attempt login.
+     *
+     * @var int
+     */
+    protected $maxAttempts = 3;
+
+    /**
+     * The number of minutes to delay further login attempts.
+     *
+     * @var int
+     */
+    protected $lockoutTime = 15;
 
     /**
      * @return string
@@ -30,5 +45,18 @@ class LoginController extends Controller
     public function show()
     {
         return view('admin.auth.login');
+    }
+
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    protected function hasTooManyLoginAttempts(Request $request)
+    {
+        return $this->limiter()->tooManyAttempts(
+            $this->throttleKey($request),
+            $this->maxAttempts,
+            $this->lockoutTime
+        );
     }
 }
