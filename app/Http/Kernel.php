4a65d61709fad62613946d39fa25382bf0509c5f
Andrei Badea <zbiller@gmail.com>
2017-02-24 20:15:44 +0200

    enable some middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
@@ -50,11 +50,15 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+        /*'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,*/
+
+        'authenticated' => \App\Http\Middleware\Authenticated::class,
+        'not.authenticated' => \App\Http\Middleware\NotAuthenticated::class,
+        'check.roles' => \App\Http\Middleware\CheckRoles::class,
+        'check.permissions' => \App\Http\Middleware\CheckPermissions::class,
     ];
 }
