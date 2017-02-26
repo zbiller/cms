28672a6a7c07e2b36da8179777697bde11ff095b
Andrei Badea <zbiller@gmail.com>
2017-02-26 17:38:32 +0200

    registered blade directives for user roles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\View\Compilers\BladeCompiler;
 use Schema;
 use App\Helpers\Menu\Menu;
 use Illuminate\Support\ServiceProvider;
@@ -25,10 +26,59 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->registerFacades();
+        $this->registerBladeExtensions();
+    }
+
+    /**
+     * @return void
+     */
+    protected function registerFacades()
+    {
         $this->app->singleton('Menu', function ($app) {
             return new Menu($app);
         });
 
         $this->app->alias('menu', Menu::class);
+    }
+
+    /**
+     * @return void
+     */
+    protected function registerBladeExtensions()
+    {
+        $this->app->afterResolving('blade.compiler', function (BladeCompiler $bladeCompiler) {
+            $bladeCompiler->directive('role', function ($role) {
+                return "<?php if(auth()->check() && auth()->user()->hasRole({$role})): ?>";
+            });
+
+            $bladeCompiler->directive('endrole', function () {
+                return '<?php endif; ?>';
+            });
+
+            $bladeCompiler->directive('hasrole', function ($role) {
+                return "<?php if(auth()->check() && auth()->user()->hasRole({$role})): ?>";
+            });
+
+            $bladeCompiler->directive('endhasrole', function () {
+                return '<?php endif; ?>';
+            });
+
+            $bladeCompiler->directive('hasanyrole', function ($roles) {
+                return "<?php if(auth()->check() && auth()->user()->hasAnyRole({$roles})): ?>";
+            });
+
+            $bladeCompiler->directive('endhasanyrole', function () {
+                return '<?php endif; ?>';
+            });
+
+            $bladeCompiler->directive('hasallroles', function ($roles) {
+                return "<?php if(auth()->check() && auth()->user()->hasAllRoles({$roles})): ?>";
+            });
+
+            $bladeCompiler->directive('endhasallroles', function () {
+                return '<?php endif; ?>';
+            });
+        });
     }
 }
