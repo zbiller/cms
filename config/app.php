d5cef1cb2a3cb6df7e9be326ac5b439bfcfe8aab
Andrei Badea <zbiller@gmail.com>
2017-02-26 19:47:31 +0200

    registered the acl service provider

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\ViewServiceProvider::class,
         App\Providers\ComposerServiceProvider::class,
+        App\Providers\AclServiceProvider::class,
 
     ],
 
