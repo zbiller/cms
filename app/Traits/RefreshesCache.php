74e28f5fdf054dbb9209ad0fdea18673520e900e
Andrei Badea <zbiller@gmail.com>
2017-03-07 22:08:00 +0200

    refactored cache refreher functionality

--- app/Traits/RefreshesCache.php
+++ app/Traits/RefreshesCache.php
@@ -8,6 +8,17 @@ use App\Exceptions\CacheException;
 trait RefreshesCache
 {
     /**
+     * The options necessary for the trait to work.
+     * This property should be declared on the models using this trait.
+     *
+     * @var array
+     */
+    protected $cache = [];
+
+    /**
+     * On every database change, attempt to clear the cache.
+     * This way, cache is kept in sync with the database table.
+     *
      * @return void
      */
     public static function bootRefreshesCache()
@@ -26,18 +37,31 @@ trait RefreshesCache
     }
 
     /**
+     * Checks for the cache key and after that delete the cache for that key.
+     *
      * @throws CacheException
      */
     public function forgetCache()
+    {
+        $this->checkKey();
+
+        cache()->forget($this->cache['key']);
+    }
+
+    /**
+     * Verify if the Model using this trait has a $cache property defined.
+     * The $cache property (array) should contain the "key" (the actual cache key name).
+     *
+     * @throws CacheException
+     */
+    private function checkKey()
     {
         if (!$this->cache || !isset($this->cache['key'])) {
             throw new CacheException(
                 'Model ' . get_class($this) . ' uses the RefreshesCache trait. ' . PHP_EOL .
                 'You have to define a "protected property $cache" (array) on that model. ' . PHP_EOL .
-                'The property should contain: ["key" => "your cache key name"]'
+                'The property should contain at least: ["key" => "your cache key name"]'
             );
         }
-
-        Cache::forget($this->cache['key']);
     }
 }
