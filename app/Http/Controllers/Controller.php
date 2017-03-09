ec7607c95137268ee6f86fe10efdd211328ae68c
Andrei Badea <zbiller@gmail.com>
2017-03-09 21:36:40 +0200

    added bootable support for controllers and their traits

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,11 +3,55 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Boot the child controller.
+     * Boot the child controller's traits.
+     *
+     * @set boot
+     */
+    public function __construct()
+    {
+        $this->bootController();
+        $this->bootTraits();
+    }
+
+    /**
+     * @return void
+     */
+    private function bootController()
+    {
+        if (method_exists($this, 'boot')) {
+            $arguments = [];
+
+            foreach ((new \ReflectionMethod($this, 'boot'))->getParameters() as $parameter) {
+                preg_match("/.*<required> (.+) \\\${$parameter->getName()}/", $parameter, $hint);
+
+                if (count($hint) == 2) {
+                    array_push($arguments, $class = app($hint[1]));
+                }
+            }
+
+            call_user_func_array([&$this, 'boot'], $arguments);
+        }
+    }
+
+    /**
+     * @return void
+     */
+    private function bootTraits()
+    {
+        foreach (class_uses_recursive($this) as $trait) {
+            if (method_exists($this, $method = 'boot' . class_basename($trait))) {
+                call_user_func([&$this, $method]);
+            }
+        }
+    }
 }
