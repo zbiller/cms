b679ea5e0c81e9910cb29795235629a87c2ce968
Andrei Badea <zbiller@gmail.com>
2017-03-16 00:19:35 +0200

    fixed header responsiveness

--- resources/layouts/admin/partials/_header.blade.php
+++ resources/layouts/admin/partials/_header.blade.php
@@ -4,7 +4,7 @@
             <i class="fa fa-bars"></i>
         </button>
         {!! form()->open(['url' => route('admin.logout'), 'method' => 'post']) !!}
-        {!! form()->button('<i class="fa fa-power-off"></i>&nbsp; Logout', ['type' => 'submit', 'class' => 'btn right']) !!}
+        {!! form()->button('<i class="fa fa-power-off"></i>&nbsp; Logout', ['type' => 'submit', 'class' => 'logout btn right']) !!}
         {!! form()->close() !!}
     @show
 </header>
