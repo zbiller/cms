63f5f936cd382bcf3bdc0a35790f1e8aac6e6332
Andrei Badea <zbiller@gmail.com>
2017-03-16 01:40:02 +0200

    local tinymce loaded asynchronously as an independent resource and not bundled by grunt

--- resources/layouts/admin/partials/_footer.blade.php
+++ resources/layouts/admin/partials/_footer.blade.php
@@ -2,8 +2,9 @@
     @yield('footer')
 </footer>
 
-{{ Html::script('//cloud.tinymce.com/stable/tinymce.min.js?apiKey=lfc3z2mhix7d8cjtwsfwz179fd41jri6qev78dn67d1kb6g5') }}
+{{--{{ Html::script('//cloud.tinymce.com/stable/tinymce.min.js?apiKey=lfc3z2mhix7d8cjtwsfwz179fd41jri6qev78dn67d1kb6g5') }}--}}
 {{ Html::script('/build/admin/js/super.min.js') }}
+{{ Html::script('/build/_async/tinymce/js/tinymce/tinymce.min.js', ['async']) }}
 
 @section('bottom_styles') @show
 @section('bottom_scripts') @show
