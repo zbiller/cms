775c046edded65f93da8d1cac329ff43375bd21a
Andrei Badea <zbiller@gmail.com>
2017-03-19 02:06:26 +0200

    created a migration for uploads entries

---
+++ database/migrations/2017_03_17_190741_create_uploads_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUploadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('uploads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unsinged()->unique();
+            $table->string('original_name');
+            $table->string('path');
+            $table->string('full_path');
+            $table->string('extension');
+            $table->integer('size');
+            $table->string('mime');
+            $table->tinyInteger('type')->default(0);
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('uploads');
+    }
+}
