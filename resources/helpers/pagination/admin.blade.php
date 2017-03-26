ad2cdea14dca083e293ca5d3b7fa3788bdbcf1ff
Andrei Badea <zbiller@gmail.com>
2017-03-26 16:19:27 +0300

    enabled admin pagination with ability to preserve query string

--- resources/helpers/pagination/admin.blade.php
+++ resources/helpers/pagination/admin.blade.php
@@ -1,36 +1,37 @@
 <section class="pagination">
-    <a href="{{ $paginator->url(1) }}" class="first {!! $paginator->onFirstPage() ? 'inactive' : '' !!}">
+    <a href="{{ $paginator->appends(request()->input())->url(1) }}" class="first {!! $paginator->onFirstPage() ? 'inactive' : '' !!}">
         <i class="fa fa-angle-double-left"></i>
     </a>
-    <a href="{{ $paginator->previousPageUrl()  }}" class="previous {!! $paginator->onFirstPage() ? 'inactive' : '' !!}">
+    <a href="{{ $paginator->appends(request()->input())->previousPageUrl()  }}" class="previous {!! $paginator->onFirstPage() ? 'inactive' : '' !!}">
         <i class="fa fa-angle-left"></i>
     </a>
 
     @php $currentPage = $paginator->currentPage(); @endphp
+
     @foreach($elements as $element)
         @if(is_array($element))
             @foreach ($element as $page => $url)
                 @if($paginator->onFirstPage())
                     @if(($page == $currentPage) || ($page == $currentPage + 1) || ($page == $currentPage + 2))
-                        <a href="{{ $url }}" class="{!! $page == $paginator->currentPage() ? 'current' : '' !!}">{{ $page }}</a>
+                        <a href="{{ $paginator->appends(request()->input())->url($page) }}" class="{!! $page == $paginator->currentPage() ? 'current' : '' !!}">{{ $page }}</a>
                     @endif
                 @elseif(!$paginator->hasMorePages())
                     @if(($page == $currentPage - 2) || ($page == $currentPage - 1) || ($page == $currentPage))
-                        <a href="{{ $url }}" class="{!! $page == $paginator->currentPage() ? 'current' : '' !!}">{{ $page }}</a>
+                        <a href="{{ $paginator->appends(request()->input())->url($page) }}" class="{!! $page == $paginator->currentPage() ? 'current' : '' !!}">{{ $page }}</a>
                     @endif
                 @else
                     @if(($page == $currentPage - 1) || ($page == $currentPage) || ($page == $currentPage + 1))
-                        <a href="{{ $url }}" class="{!! $page == $paginator->currentPage() ? 'current' : '' !!}">{{ $page }}</a>
+                        <a href="{{ $paginator->appends(request()->input())->url($page) }}" class="{!! $page == $paginator->currentPage() ? 'current' : '' !!}">{{ $page }}</a>
                     @endif
                 @endif
             @endforeach
         @endif
     @endforeach
 
-    <a href="{{ $paginator->nextPageUrl() }}" class="next {!! $paginator->hasMorePages() ? '' : 'inactive' !!}">
+    <a href="{{ $paginator->appends(request()->input())->nextPageUrl() }}" class="next {!! $paginator->hasMorePages() ? '' : 'inactive' !!}">
         <i class="fa fa-angle-right"></i>
     </a>
-    <a href="{{ $paginator->url($paginator->lastPage()) }}" class="last {!! $paginator->hasMorePages() ? '' : 'inactive' !!}">
+    <a href="{{ $paginator->appends(request()->input())->url($paginator->lastPage()) }}" class="last {!! $paginator->hasMorePages() ? '' : 'inactive' !!}">
         <i class="fa fa-angle-double-right"></i>
     </a>
 
