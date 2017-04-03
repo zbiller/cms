f6ce33aa1ef9e0554a6afa47aded791eba81ef29
Andrei Badea <zbiller@gmail.com>
2017-04-03 22:03:56 +0300

    modified admin to load independent scripts from build/plugins

--- resources/layouts/admin/partials/_footer.blade.php
+++ resources/layouts/admin/partials/_footer.blade.php
@@ -2,9 +2,8 @@
     @yield('footer')
 </footer>
 
-{{--{{ Html::script('//cloud.tinymce.com/stable/tinymce.min.js?apiKey=lfc3z2mhix7d8cjtwsfwz179fd41jri6qev78dn67d1kb6g5') }}--}}
 {{ Html::script('/build/admin/js/super.min.js') }}
-{{ Html::script('/build/_async/tinymce/js/tinymce/tinymce.min.js', ['async']) }}
+{{ Html::script('/build/plugins/tinymce/js/tinymce/tinymce.min.js', ['async']) }}
 
 @section('bottom_styles') @show
 @section('bottom_scripts') @show
