e6c69137c484ef7800ed7c43862c7ce8d4ef222a
Andrei Badea <zbiller@gmail.com>
2017-04-24 00:18:13 +0300

    hooked blocks composers and view paths into the application

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Cms\Block;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 class ComposerServiceProvider extends ServiceProvider
@@ -13,6 +14,12 @@ class ComposerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        /* link admin menu composer */
         view()->composer('layouts::admin.partials._menu', 'App\Http\Composers\MenuComposer@admin');
+
+        /* link blocks composers */
+        foreach (Block::$map as $type => $options) {
+            view()->composer("blocks_{$type}::front", $options['composer_class']);
+        }
     }
 }
--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Cms\Block;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 class ViewServiceProvider extends ServiceProvider
@@ -13,9 +14,15 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (!app()->runningInConsole()) {
-            view()->addNamespace('layouts', config('view.paths.layouts'));
-            view()->addNamespace('helpers', config('view.paths.helpers'));
+        /* setup layouts view path */
+        view()->addNamespace('layouts', config('view.paths.layouts'));
+
+        /* setup helpers view path */
+        view()->addNamespace('helpers', config('view.paths.helpers'));
+
+        /* setup blocks view paths */
+        foreach (Block::$map as $type => $options) {
+            view()->addNamespace("blocks_{$type}", realpath(base_path($options['views_path'])));
         }
     }
 }
