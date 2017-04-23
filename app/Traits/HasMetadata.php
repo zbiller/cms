d55c2b1c519a7a58b98e91641440a123e9db2759
Andrei Badea <zbiller@gmail.com>
2017-04-24 00:16:45 +0300

    enhanced the metadata trait to be able to get a jsonable value from an array format

--- app/Traits/HasMetadata.php
+++ app/Traits/HasMetadata.php
@@ -23,4 +23,21 @@ trait HasMetadata
     {
         $this->attributes['metadata'] = $value ? json_encode($value) : null;
     }
+
+    /**
+     * Get a metadata property from a raw field representation.
+     * It can get the json representation from something like: metadata[items][0][name]
+     *
+     * metadata[items][0][name] to metadata->items->0->name
+     *
+     * @param string $raw
+     * @return mixed
+     */
+    public function metadata($raw)
+    {
+        $metadata = get_object_vars_recursive($this->metadata);
+        $field = str_replace('][', '.', trim(str_replace('metadata', '', $raw), '.[]'));
+
+        return array_get($metadata, $field);
+    }
 }
