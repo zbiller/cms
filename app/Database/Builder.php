dd2be3095b50e065063e61377e54ae8a907416f6
Andrei Badea <zbiller@gmail.com>
2017-06-10 01:37:14 +0300

    created custom database builder to automatically cache already executed queries

--- app/Database/Builder.php
+++ app/Database/Builder.php
@@ -2,25 +2,47 @@
 
 namespace App\Database;
 
-use Cache;
 use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\ConnectionInterface;
+use Illuminate\Database\Query\Grammars\Grammar;
+use Illuminate\Database\Query\Processors\Processor;
 
 class Builder extends QueryBuilder
 {
     /**
-     * Run the query as a "select" statement against the connection.
-     *
-     * @return array
+     * @var string
      */
-    protected function runSelect()
+    protected $cacheTag;
+
+    /**
+     * Create a new query builder instance.
+     * Instantiate the cache tag value to be later used by Redis when storing the query in cache.
+     *
+     * @param ConnectionInterface $connection
+     * @param Grammar|null $grammar
+     * @param Processor|null $processor
+     * @param string|null $cacheTag
+     */
+    public function __construct(ConnectionInterface $connection, Grammar $grammar = null, Processor $processor = null, $cacheTag = null)
     {
-        return Cache::store('array')->remember($this->getCacheKey(), 1, function() {
-            return parent::runSelect();
-        });
+        parent::__construct($connection, $grammar, $processor);
+
+        $this->cacheTag = $cacheTag;
     }
 
     /**
-     * Returns a Unique String that can identify this Query.
+     * Returns a cache tag for Redis to use.
+     * This tag is used so we know for which model/database_table the cache was set in Redis.
+     *
+     * @return string
+     */
+    protected function getCacheTag()
+    {
+        return $this->cacheTag;
+    }
+
+    /**
+     * Returns a unique string that can identify this query.
      *
      * @return string
      */
@@ -29,5 +51,42 @@ class Builder extends QueryBuilder
         return json_encode([
             $this->toSql() => $this->getBindings()
         ]);
+    }
+
+    /**
+     * Run the query as a "select" statement against the connection.
+     *
+     * @return array
+     */
+    protected function runSelect()
+    {
+        return cache()->store('redis')->tags($this->getCacheTag())->rememberForever($this->getCacheKey(), function() {
+            return parent::runSelect();
+        });
+    }
+
+    /**
+     * Delete a record from the database.
+     *
+     * @param int|null $id
+     * @return int
+     */
+    public function delete($id = null)
+    {
+        cache()->store('redis')->tags($this->getCacheTag())->flush();
+
+        parent::delete($id);
+    }
+
+    /**
+     * Run a truncate statement on the table.
+     *
+     * @return void
+     */
+    public function truncate()
+    {
+        cache()->store('redis')->tags($this->getCacheTag())->flush();
+
+        parent::truncate();
     }
 }
